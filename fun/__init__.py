35f78d20a942717d8fd4c71f214033a726972a5b
lfmbigc <lfmbigc@gmail.com>
2018-09-03 20:41:55 +0800

    Add files via upload

---
+++ fun/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  3 19:32:46 2018
+
+@author: liufengming
+"""
+
+def joke():
+    return ("你好！")
+    
+    
---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  3 19:35:00 2018
+
+@author: liufengming
+"""
+
+from distutils.core import setup
+
+setup(name='fun',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/wodeusername',
+      author='wodeusername',
+      author_email='wode@example.com',
+      license='None',
+      packages=['fun'],
+      zip_safe=False)
